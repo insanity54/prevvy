7f9ef063cbc057344fa38afd11690e6d363498c0
Chris Grimmett <chris@grimtech.net>
2020-12-23 01:02:25 -0800

    generation works! dont touch it!

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,23 +1,80 @@
-const prevvy = require('../index');
+const Prevvy = require('../index');
 const path = require('path');
 const testVideoPath = path.join(__dirname, 'testVideo.mp4');
-const testImagePath = path.join(__dirname, 'testImage.jpg');
+const testImage3x3Path = path.join(__dirname, 'testImage3x3.png');
+const testImage6x3Path = path.join(__dirname, 'testImage6x3.png');
+
+const opts1 = {
+  input: testVideoPath,
+  output: testImage3x3Path,
+  width: 256,
+  cols: 3,
+  rows: 3
+}
 
 describe('prevvy', () => {
-  xit('should throw if not receiving params', () => {
+  jest.setTimeout(10000);
+  describe('generate', () => {
+    xit('should throw if not receiving params', () => {
 
-  })
-  xit('should generate an image faster than ffmpeg-generate-video-preview', () => {
+    })
+    xit('should generate an image faster than ffmpeg-generate-video-preview', () => {
 
-  })
-  it('should be a good boy', () => {
-    const opts = {
-      input: testVideoPath,
-      output: testImagePath,
-      cols: 6,
-      rows: 3,
-      width: 256
-    }
-    return prevvy(opts)
-  })
+    })
+    test('should make a 3x3 preview image', () => {
+      const opts = {
+        input: testVideoPath,
+        output: testImage3x3Path,
+        cols: 3,
+        rows: 3,
+        width: 256
+      }
+      let p = new Prevvy(opts);
+      return p.generate();
+    })
+    test('should make a 6x3 preview image', () => {
+      const opts = {
+        input: testVideoPath,
+        output: testImage6x3Path,
+        cols: 6,
+        rows: 3,
+        width: 256
+      }
+      let p = new Prevvy(opts);
+      return p.generate();
+    })
+  });
+
+  describe('makeLayout', () => {
+    beforeEach(() => {
+      p = new Prevvy(opts1);
+    })
+    test('3x3 grid i 0', () => {
+      expect(p.makeLayout(0)).toBe('0_0');
+    })
+    test('3x3 grid i 1', () => {
+      expect(p.makeLayout(1)).toBe('w0_0');
+    })
+    test('3x3 grid i 2', () => {
+      expect(p.makeLayout(2)).toBe('w0+w0_0');
+    })
+    test('3x3 grid i 3', () => {
+      expect(p.makeLayout(3)).toBe('0_h0');
+    })
+    test('3x3 grid i 4', () => {
+      expect(p.makeLayout(4)).toBe('w0_h0');
+    })
+    test('3x3 grid i 5', () => {
+      expect(p.makeLayout(5)).toBe('w0+w0_h0');
+    })
+    test('3x3 grid i 6', () => {
+      expect(p.makeLayout(6)).toBe('0_h0+h0');
+    })
+    test('3x3 grid i 7', () => {
+      expect(p.makeLayout(7)).toBe('w0_h0+h0');
+    })
+    test('3x3 grid i 8', () => {
+      expect(p.makeLayout(8)).toBe('w0+w0_h0+h0');
+    });
+  });
 });
--- index.js
+++ index.js
@@ -5,61 +5,82 @@ const Promise = require('bluebird');
 const os = require('os');
 const path = require('path');
 
-const tmpDir = path.join(__dirname, 'tmp');
 
-module.exports = async (options) => {
-  const { input, output, width, cols, rows } = options;
-
-
-
-  // get the length of the video
-  const info = await probe(input);
-  console.log(`input.duration:${info.duration}`);
-
-  // determine the tile count
-  const tileCount = cols*rows;
-
-  // use ffmpeg to get equidistant snapshots
-  const msSlice = parseInt(info.duration/tileCount);
-  console.log(`${msSlice}, ${tileCount}`);
-
-  let framePromises = [];
-  for (var i=0; i<tileCount; i++) {
-    const timestamp = Duration.fromMillis(i*msSlice).toFormat('h:m:s');
-    const intermediateOutput = path.join(tmpDir, `prevvy_intermediate${i}.png`);
-    framePromises.push(execa.command(`/usr/bin/ffmpeg -y -ss ${timestamp} -i ${input} -frames:v 1 ${intermediateOutput}`));
+class Prevvy {
+  constructor(opts) {
+    this.tmpDir = path.join(__dirname, 'tmp'); // @todo change to os.tmpdir()
+    this.input = opts.input;
+    this.output = opts.output;
+    this.cols = opts.cols;
+    this.rows = opts.rows;
+    this.width = opts.width;
+    this.tileCount = this.rows*this.cols;
+    if (typeof this.input === 'undefined') throw new Error('input is required in options. got undefined.')
+    if (typeof this.output === 'undefined') throw new Error('output is required in options. got undefined.')
+    if (typeof this.cols === 'undefined') throw new Error('cols is required in options. got undefined.')
+    if (typeof this.rows === 'undefined') throw new Error('rows is required in options. got undefined.')
+    if (typeof this.width === 'undefined') throw new Error('width is required in options. got undefined.')
   }
 
-
-  let result = await Promise.all(framePromises);
-
-  const makeLayout = (i, t, c, r) => {
-    const currentColumn = Math.floor(i/r);
-    const currentRow = Math.floor(i/c);
-    console.log(`makeLayout ${i} is on column ${currentColumn}`)
-    return `w${i}_${0}`
-    // `0_0|w0_0|0_h0|w0_h0|w1_1|1_h1|w1_h1`
-
-    // 0  1  2  3  4   5
-    // 6  7  8  9  10  11
-    // 12 13 14 15 16  17
-
+  makeLayout (i) {
+    // see https://ffmpeg.org/ffmpeg-filters.html#xstack for the madness
+    const currentColumn = i%this.cols;
+    const currentRow = Math.floor(i/this.cols);
+    let colSide = [];
+    let rowSide = [];
+    if (currentColumn === 0) {
+      colSide.push('0');
+    } else {
+      for (var j = 0; j<currentColumn; j++) {
+        colSide.push('w0');
+      }
+    }
+    if (currentRow === 0) {
+      rowSide.push('0');
+    } else {
+      for (var j = 0; j<currentRow; j++) {
+        rowSide.push('h0');
+      }
+    }
+    return `${colSide.join('+')}_${rowSide.join('+')}`;
   }
 
-  // combine images together to make tile
-  let inputFiles = [];
-  let map = [];
-  let layout = [];
-  for (var i=0; i<tileCount; i++) {
-    inputFiles.push(`-i ${tmpDir}/prevvy_intermediate${i}.png `);
-    map.push(`[${i}:v]`);
-    layout.push(makeLayout(i, tileCount, cols, rows));
+  async generate () {
+    // get the length of the video
+    const info = await probe(this.input);
+
+    // use ffmpeg to get equidistant snapshots
+    const msSlice = parseInt(info.duration/this.tileCount);
+
+    let framePromises = [];
+    for (var i=0; i<this.tileCount; i++) {
+      const timestamp = Duration.fromMillis(i*msSlice).toFormat('h:m:s');
+      const intermediateOutput = path.join(this.tmpDir, `prevvy_intermediate${i}.png`);
+      framePromises.push(execa.command(`/usr/bin/ffmpeg -y -ss ${timestamp} -i ${this.input} -frames:v 1 ${intermediateOutput}`));
+    }
+
+
+    let result = await Promise.all(framePromises);
+
+    // combine images together to make tile
+    let inputFiles = [];
+    let map = [];
+    let layout = [];
+    for (var i=0; i<this.tileCount; i++) {
+      inputFiles.push(`-i ${this.tmpDir}/prevvy_intermediate${i}.png `);
+      map.push(`[${i}:v]`);
+      layout.push(this.makeLayout(i));
+    }
+
+
+
+    return execa.command(`/usr/bin/ffmpeg -y ${inputFiles.join(' ')}-filter_complex ${map.join('')}xstack=inputs=${this.tileCount}:layout=${layout.join('|')}[v] -map [v] ${this.output}`);
+    // /usr/bin/ffmpeg -y -i ./tmp/intermediate0.png -i ./tmp/intermediate1.png -i ./tmp/intermediate2.png -i ./tmp/intermediate3.png -i ./tmp/intermediate4.png -i ./tmp/intermediate5.png -i ./tmp/intermediate6.png -i ./tmp/intermediate7.png -i ./tmp/intermediate8.png -i ./tmp/intermediate8.png -filter_complex "[0:v][1:v][2:v][3:v][4:v][5:v][6:v][7:v][8:v]xstack=inputs=9:layout=0_0|0_h0|0_h0+h1|w0_0|w0_h0|w0_h0+h1|w0+w3_0|w0+w3_h0|w0+w3_h0+h1[v]" -map [v] output.png
+    // return image
   }
-
-  console.log(layout)
-
-
-
-  return execa.command(`/usr/bin/ffmpeg -y ${inputFiles.join(' ')}-filter_complex ${map.join('')}xstack=inputs=${tileCount}:layout=${layout.join('|')}[v] -map [v] ${output}`);
-  // return image
 }
+
+
+
+
+module.exports = Prevvy;
