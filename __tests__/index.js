0e4fa8854c98f084354997d3d0cd664b1f4b90d7
Chris Grimmett <chris@grimtech.net>
2023-02-26 05:04:05 -0800

    switch to esm

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,141 +1,141 @@
-const Prevvy = require('../index');
-const generatePreview = require('ffmpeg-generate-video-preview');
-const path = require('path');
-const fs = require('fs');
-const os = require('os');
-
+import Prevvy from "../index.js";
+import * as generatePreview from "ffmpeg-generate-video-preview";
+import path from "path";
+import fs from "fs";
+import os from "os";
 const testVideoPath = path.join(__dirname, 'video.mp4');
 const testImageDir = __dirname;
 const testImage3x3Path = path.join(__dirname, 'testImage3x3.png');
 const testImage6x3Path = path.join(__dirname, 'testImage6x3.png');
 const movieUrl = 'https://ipfs.io/ipfs/QmTKZgRNwDNZwHtJSjCp6r5FYefzpULfy37JvMt9DwvXse/video.mp4';
 const opts = (id) => {
-  return {
-    input: testVideoPath,
-    output: path.join(testImageDir, `testImage_${id}.png`),
-    width: 256,
-    cols: 3,
-    rows: 3
-  };
+    return {
+        input: testVideoPath,
+        output: path.join(testImageDir, `testImage_${id}.png`),
+        width: 256,
+        cols: 3,
+        rows: 3
+    };
 };
-
-
 const assertTestVideoExistence = () => {
-  return new Promise((resolve, reject) => {
-    fs.stat(testVideoPath, (err, stats) => {
-      if (typeof stats === 'undefined') {
-        reject("The test videos don\'t exist. Please run 'yarn run assets' to fetch them.");
-      } else {
-        resolve();
-      }
+    return new Promise((resolve, reject) => {
+        fs.stat(testVideoPath, (err, stats) => {
+            if (typeof stats === 'undefined') {
+                reject("The test videos don\'t exist. Please run 'yarn run assets' to fetch them.");
+            }
+            else {
+                resolve();
+            }
+        });
     });
-  })
-}
-
-
+};
 describe('prevvy', () => {
-  jest.setTimeout(10000);
-  beforeAll(() => {
-    return assertTestVideoExistence();
-  });
-  describe('perf', () => {
-    xit('should generate an image faster than ffmpeg-generate-video-preview', async () => {
-      // this fails on videos of lower file size, but I think it will succeed on videos that are several GB in size.
-      // There are complications getting a large filesize video to test (my internet sucks)
-
-      let prevvyStartTime = new Date().valueOf();
-      let p = new Prevvy(opts('prevvy'));
-      await p.generate();
-      let prevvyEndTime = new Date().valueOf();
-      let prevvyElapsedTime = prevvyEndTime-prevvyStartTime;
-      console.log(`prevvy took ${prevvyElapsedTime}ms`);
-
-      let fgvpStartTime = new Date().valueOf();
-      await generatePreview(opts('fgvp'));
-      let fgvpEndTime = new Date().valueOf();
-      let fgvpElapsedTime = fgvpEndTime-fgvpStartTime;
-      console.log(`fgvp took ${fgvpElapsedTime}ms`);
-
-      expect(prevvyElapsedTime).toBeLessThan(fgvpElapsedTime);
-
-    })
-  })
-  describe('generate', () => {
-    test('http input source', () => {
-      const opts = {
-        input: 'https://ipfs.io/ipfs/bafkreifufx6uharnts5wy6smk7mxmlwg7fpzhf5s3n33kydfgr7zqhagme?filename=test-30211016T000000Z.mp4',
-        output: path.join(os.tmpdir(), 'test.png'),
-        cols: 3,
-        rows: 3,
-        width: 42,
-        throttleTimeout: 10000
-      }
-      let p = new Prevvy(opts);
-      return p.generate();
-    }, 120000)
-    test('should cope with a video with spaces and special characters in its name', () => {
-      const opts = {
-        input: path.join(__dirname, 'test vid (spaces).mp4'),
-        output: path.join(__dirname, 'spacesOutput.png'),
-        cols: 3,
-        rows: 3,
-        width: 42
-      }
+    jest.setTimeout(10000);
+    beforeAll(() => {
+        return assertTestVideoExistence();
     });
-    test('should make a 3x3 preview image', () => {
-      const opts = {
-        input: testVideoPath,
-        output: testImage3x3Path,
-        cols: 3,
-        rows: 3,
-        width: 256
-      }
-      let p = new Prevvy(opts);
-      return p.generate();
-    })
-    test('should make a 6x3 preview image', () => {
-      const opts = {
-        input: testVideoPath,
-        output: testImage6x3Path,
-        cols: 6,
-        rows: 3,
-        width: 128
-      }
-      let p = new Prevvy(opts);
-      return p.generate();
-    })
-  });
-
-  describe('makeLayout', () => {
-    beforeEach(() => {
-      p = new Prevvy(opts('layout'));
-    })
-    test('3x3 grid i 0', () => {
-      expect(p.makeLayout(0)).toBe('0_0');
-    })
-    test('3x3 grid i 1', () => {
-      expect(p.makeLayout(1)).toBe('w0_0');
-    })
-    test('3x3 grid i 2', () => {
-      expect(p.makeLayout(2)).toBe('w0+w0_0');
-    })
-    test('3x3 grid i 3', () => {
-      expect(p.makeLayout(3)).toBe('0_h0');
-    })
-    test('3x3 grid i 4', () => {
-      expect(p.makeLayout(4)).toBe('w0_h0');
-    })
-    test('3x3 grid i 5', () => {
-      expect(p.makeLayout(5)).toBe('w0+w0_h0');
-    })
-    test('3x3 grid i 6', () => {
-      expect(p.makeLayout(6)).toBe('0_h0+h0');
-    })
-    test('3x3 grid i 7', () => {
-      expect(p.makeLayout(7)).toBe('w0_h0+h0');
-    })
-    test('3x3 grid i 8', () => {
-      expect(p.makeLayout(8)).toBe('w0+w0_h0+h0');
+    xdescribe('sanity', () => {
+        it('should give human-readable format if ffprobe does not exist/is not accessible in the running environment', async () => {
+            jest.mock('ffprobe', () => {
+                return jest.fn(() => 42);
+            });
+            const p = new Prevvy();
+            await expect(p.ffprobeP('video.mp4')).to.throw(/environment/);
+        });
     });
-  });
+    describe('perf', () => {
+        xit('should generate an image faster than ffmpeg-generate-video-preview', async () => {
+            // this fails on videos of lower file size, but I think it will succeed on videos that are several GB in size.
+            // There are complications getting a large filesize video to test (my internet sucks)
+            let prevvyStartTime = new Date().valueOf();
+            let p = new Prevvy(opts('prevvy'));
+            await p.generate();
+            let prevvyEndTime = new Date().valueOf();
+            let prevvyElapsedTime = prevvyEndTime - prevvyStartTime;
+            console.log(`prevvy took ${prevvyElapsedTime}ms`);
+            let fgvpStartTime = new Date().valueOf();
+            await generatePreview(opts('fgvp'));
+            let fgvpEndTime = new Date().valueOf();
+            let fgvpElapsedTime = fgvpEndTime - fgvpStartTime;
+            console.log(`fgvp took ${fgvpElapsedTime}ms`);
+            expect(prevvyElapsedTime).toBeLessThan(fgvpElapsedTime);
+        });
+    });
+    describe('generate', () => {
+        test('http input source', () => {
+            const opts = {
+                input: 'https://ipfs.io/ipfs/bafkreifufx6uharnts5wy6smk7mxmlwg7fpzhf5s3n33kydfgr7zqhagme?filename=test-30211016T000000Z.mp4',
+                output: path.join(os.tmpdir(), 'test.png'),
+                cols: 3,
+                rows: 3,
+                width: 42,
+                throttleTimeout: 10000
+            };
+            let p = new Prevvy(opts);
+            return p.generate();
+        }, 120000);
+        test('should cope with a video with spaces and special characters in its name', () => {
+            const opts = {
+                input: path.join(__dirname, 'test vid (spaces).mp4'),
+                output: path.join(__dirname, 'spacesOutput.png'),
+                cols: 3,
+                rows: 3,
+                width: 42
+            };
+        });
+        test('should make a 3x3 preview image', () => {
+            const opts = {
+                input: testVideoPath,
+                output: testImage3x3Path,
+                cols: 3,
+                rows: 3,
+                width: 256
+            };
+            let p = new Prevvy(opts);
+            return p.generate();
+        });
+        test('should make a 6x3 preview image', () => {
+            const opts = {
+                input: testVideoPath,
+                output: testImage6x3Path,
+                cols: 6,
+                rows: 3,
+                width: 128
+            };
+            let p = new Prevvy(opts);
+            return p.generate();
+        });
+    });
+    describe('makeLayout', () => {
+        beforeEach(() => {
+            p = new Prevvy(opts('layout'));
+        });
+        test('3x3 grid i 0', () => {
+            expect(p.makeLayout(0)).toBe('0_0');
+        });
+        test('3x3 grid i 1', () => {
+            expect(p.makeLayout(1)).toBe('w0_0');
+        });
+        test('3x3 grid i 2', () => {
+            expect(p.makeLayout(2)).toBe('w0+w0_0');
+        });
+        test('3x3 grid i 3', () => {
+            expect(p.makeLayout(3)).toBe('0_h0');
+        });
+        test('3x3 grid i 4', () => {
+            expect(p.makeLayout(4)).toBe('w0_h0');
+        });
+        test('3x3 grid i 5', () => {
+            expect(p.makeLayout(5)).toBe('w0+w0_h0');
+        });
+        test('3x3 grid i 6', () => {
+            expect(p.makeLayout(6)).toBe('0_h0+h0');
+        });
+        test('3x3 grid i 7', () => {
+            expect(p.makeLayout(7)).toBe('w0_h0+h0');
+        });
+        test('3x3 grid i 8', () => {
+            expect(p.makeLayout(8)).toBe('w0+w0_h0+h0');
+        });
+    });
 });
--- index.js
+++ index.js
@@ -1,157 +1,146 @@
-const ffmpeg = require('fluent-ffmpeg');
-const { DateTime, Duration } = require('luxon');
-const Promise = require('bluebird');
-const os = require('os');
-const path = require('path');
-const debug = require('debug')('prevvy');
-const execa = require('execa');
-
+import ffmpeg from "fluent-ffmpeg";
+import { DateTime, Duration } from "luxon";
+import Promise from "bluebird";
+import os from "os";
+import path from "path";
+// import * as debug$0 from "debug";
+import execa from "execa";
+// const debug = debug$0('prevvy');
 class Prevvy {
-  constructor(opts) {
-    this.tmpDir = os.tmpdir();
-    this.input = opts.input;
-    this.output = opts.output;
-    this.cols = opts.cols;
-    this.rows = opts.rows;
-    this.width = opts.width;
-    this.tileCount = this.rows*this.cols;
-    this.throttleTimeout = opts.throttleTimeout || 1000;
-    if (typeof this.input === 'undefined') throw new Error('input is required in options. got undefined.')
-    if (typeof this.output === 'undefined') throw new Error('output is required in options. got undefined.')
-    if (typeof this.cols === 'undefined') throw new Error('cols is required in options. got undefined.')
-    if (typeof this.rows === 'undefined') throw new Error('rows is required in options. got undefined.')
-    if (typeof this.width === 'undefined') throw new Error('width is required in options. got undefined.')
-  }
-
-  makeLayout (i) {
-    // see https://ffmpeg.org/ffmpeg-filters.html#xstack for the madness
-    const currentColumn = i%this.cols;
-    const currentRow = Math.floor(i/this.cols);
-    let colSide = [];
-    let rowSide = [];
-    if (currentColumn === 0) {
-      colSide.push('0');
-    } else {
-      for (var j = 0; j<currentColumn; j++) {
-        colSide.push('w0');
-      }
+    constructor(opts) {
+        this.tmpDir = os.tmpdir();
+        this.input = opts.input;
+        this.output = opts.output;
+        this.cols = opts.cols;
+        this.rows = opts.rows;
+        this.width = opts.width;
+        this.tileCount = this.rows * this.cols;
+        this.throttleTimeout = opts.throttleTimeout || 1000;
+        if (typeof this.input === 'undefined')
+            throw new Error('input is required in options. got undefined.');
+        if (typeof this.output === 'undefined')
+            throw new Error('output is required in options. got undefined.');
+        if (typeof this.cols === 'undefined')
+            throw new Error('cols is required in options. got undefined.');
+        if (typeof this.rows === 'undefined')
+            throw new Error('rows is required in options. got undefined.');
+        if (typeof this.width === 'undefined')
+            throw new Error('width is required in options. got undefined.');
     }
-    if (currentRow === 0) {
-      rowSide.push('0');
-    } else {
-      for (var j = 0; j<currentRow; j++) {
-        rowSide.push('h0');
-      }
+    makeLayout(i) {
+        // see https://ffmpeg.org/ffmpeg-filters.html#xstack for the madness
+        const currentColumn = i % this.cols;
+        const currentRow = Math.floor(i / this.cols);
+        let colSide = [];
+        let rowSide = [];
+        if (currentColumn === 0) {
+            colSide.push('0');
+        }
+        else {
+            for (var j = 0; j < currentColumn; j++) {
+                colSide.push('w0');
+            }
+        }
+        if (currentRow === 0) {
+            rowSide.push('0');
+        }
+        else {
+            for (var j = 0; j < currentRow; j++) {
+                rowSide.push('h0');
+            }
+        }
+        return `${colSide.join('+')}_${rowSide.join('+')}`;
     }
-    return `${colSide.join('+')}_${rowSide.join('+')}`;
-  }
-
-
-  async ffmpegSeekP (timestamp, intermediateOutput) {
-    return new Promise((resolve, reject) => {
-      ffmpeg()
-        .addOption('-ss', timestamp)
-        .addOption('-i', this.input)
-        .addOption('-frames:v', '1')
-        .on('start', (cmd) => { debug(`Spawned ffmpeg with command ${cmd}`) })
-        .on('end', () => {
-          setTimeout(() => {
-            resolve(intermediateOutput);
-          }, this.throttleTimeout); // throttle for http.
-        })
-        .on('error', function(e) {
-          debug(e);
-          reject(e);
-        })
-        .save(intermediateOutput);
-    })
-  }
-
-  async ffmpegCombineP (inputFiles, streams, layouts) {
-    return new Promise((resolve, reject) => {
-      const command = ffmpeg();
-      inputFiles.forEach((inputFile) => {
-        command.input(inputFile);
-      })
-      command
-        .addOption('-y')
-        .addOption('-filter_complex', `${streams.join('')}xstack=inputs=${this.tileCount}:layout=${layouts.join('|')}[v];[v]scale=${Math.floor(this.width*this.cols)}:-1[scaled]`)
-        .addOption('-map', '[scaled]')
-        .on('start', (cmd) => debug(`Spawned ffmpeg with command ${cmd}`))
-        .on('end', function() {
-          resolve();
-        })
-        .on('error', function(e) {
-          debug(e);
-          reject(e);
-        })
-        .save(this.output);
-    })
-  }
-
-  async ffprobeP (input) {
-    return new Promise((resolve, reject) => {
-      ffmpeg.ffprobe(input, (err, data) => {
-        if (err) reject(err);
-        else resolve(data);
-      });
-    });
-  }
-
-  async getVideoDurationInSeconds (videoFilePath) {
-    const { stdout } = await execa('ffprobe', ['-v', 'error', '-show_format', '-show_streams', videoFilePath]);
-    const matched = stdout.match(/duration="?(\d*\.\d*)"?/)
-    if (matched && matched[1]) return parseFloat(matched[1])
-  }
-
-  async generate () {
-    // get the length of the video
-    const durationS = await this.getVideoDurationInSeconds(this.input);
-
-    const durationMs = durationS * 1000;
-    debug(`durationMs: ${durationMs}, durationS: ${durationS}`)
-
-    // use ffmpeg to get equidistant snapshots
-    const msSlice = parseInt(durationMs/this.tileCount);
-
-    let frameData = [];
-    for (var i=0; i<this.tileCount; i++) {
-      const timestamp = Duration.fromMillis(i*msSlice).toFormat('h:m:s');
-      const intermediateOutput = path.join(this.tmpDir, `prevvy_intermediate${i}.png`);
-      frameData.push([timestamp, intermediateOutput]);
+    async ffmpegSeekP(timestamp, intermediateOutput) {
+        return new Promise((resolve, reject) => {
+            ffmpeg()
+                .addOption('-ss', timestamp)
+                .addOption('-i', this.input)
+                .addOption('-frames:v', '1')
+                // .on('start', (cmd) => { 
+                //   debug(`Spawned ffmpeg with command ${cmd}`); 
+                // })
+                .on('end', () => {
+                setTimeout(() => {
+                    resolve(intermediateOutput);
+                }, this.throttleTimeout); // throttle for http.
+            })
+                .on('error', function (e) {
+                // debug(e);
+                reject(e);
+            })
+                .save(intermediateOutput);
+        });
     }
-
-    // throttle https requests (concurrency 1)
-    let result = await Promise.map(
-      frameData,
-      (data) => { 
-        return this.ffmpegSeekP(data[0], data[1])
-      },
-      { concurrency: (/^http/.test(this.input)) ? 1 : 64 }
-    )
-    debug(result)
-
-    // combine images together to make tile
-    let inputFiles = [];
-    let streams = [];
-    let layouts = [];
-    for (var i=0; i<this.tileCount; i++) {
-      inputFiles.push(`${this.tmpDir}/prevvy_intermediate${i}.png`);
-      streams.push(`[${i}:v]`);
-      layouts.push(this.makeLayout(i));
+    async ffmpegCombineP(inputFiles, streams, layouts) {
+        return new Promise((resolve, reject) => {
+            const command = ffmpeg();
+            inputFiles.forEach((inputFile) => {
+                command.input(inputFile);
+            });
+            command
+                .addOption('-y')
+                .addOption('-filter_complex', `${streams.join('')}xstack=inputs=${this.tileCount}:layout=${layouts.join('|')}[v];[v]scale=${Math.floor(this.width * this.cols)}:-1[scaled]`)
+                .addOption('-map', '[scaled]')
+                // .on('start', (cmd) => debug(`Spawned ffmpeg with command ${cmd}`))
+                .on('end', function () {
+                resolve();
+            })
+                .on('error', function (e) {
+                // debug(e);
+                reject(e);
+            })
+                .save(this.output);
+        });
     }
-    await this.ffmpegCombineP(inputFiles, streams, layouts);
-
-    return {
-      output: this.output
+    async ffprobeP(input) {
+        return new Promise((resolve, reject) => {
+            ffmpeg.ffprobe(input, (err, data) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve(data);
+            });
+        });
     }
-
-
-    // /usr/bin/ffmpeg -y -i ./tmp/intermediate0.png -i ./tmp/intermediate1.png -i ./tmp/intermediate2.png -i ./tmp/intermediate3.png -i ./tmp/intermediate4.png -i ./tmp/intermediate5.png -i ./tmp/intermediate6.png -i ./tmp/intermediate7.png -i ./tmp/intermediate8.png -i ./tmp/intermediate8.png -filter_complex "[0:v][1:v][2:v][3:v][4:v][5:v][6:v][7:v][8:v]xstack=inputs=9:layout=0_0|0_h0|0_h0+h1|w0_0|w0_h0|w0_h0+h1|w0+w3_0|w0+w3_h0|w0+w3_h0+h1[v]" -map [v] output.png
-  }
+    async getVideoDurationInSeconds(videoFilePath) {
+        const { stdout } = await execa('ffprobe', ['-v', 'error', '-show_format', '-show_streams', videoFilePath]);
+        const matched = stdout.match(/duration="?(\d*\.\d*)"?/);
+        if (matched && matched[1])
+            return parseFloat(matched[1]);
+    }
+    async generate() {
+        // get the length of the video
+        const durationS = await this.getVideoDurationInSeconds(this.input);
+        const durationMs = durationS * 1000;
+        // debug(`durationMs: ${durationMs}, durationS: ${durationS}`);
+        // use ffmpeg to get equidistant snapshots
+        const msSlice = parseInt(durationMs / this.tileCount);
+        let frameData = [];
+        for (var i = 0; i < this.tileCount; i++) {
+            const timestamp = Duration.fromMillis(i * msSlice).toFormat('h:m:s');
+            const intermediateOutput = path.join(this.tmpDir, `prevvy_intermediate${i}.png`);
+            frameData.push([timestamp, intermediateOutput]);
+        }
+        // throttle https requests (concurrency 1)
+        let result = await Promise.map(frameData, (data) => {
+            return this.ffmpegSeekP(data[0], data[1]);
+        }, { concurrency: (/^http/.test(this.input)) ? 1 : 64 });
+        // debug(result);
+        // combine images together to make tile
+        let inputFiles = [];
+        let streams = [];
+        let layouts = [];
+        for (var i = 0; i < this.tileCount; i++) {
+            inputFiles.push(`${this.tmpDir}/prevvy_intermediate${i}.png`);
+            streams.push(`[${i}:v]`);
+            layouts.push(this.makeLayout(i));
+        }
+        await this.ffmpegCombineP(inputFiles, streams, layouts);
+        return {
+            output: this.output
+        };
+        // /usr/bin/ffmpeg -y -i ./tmp/intermediate0.png -i ./tmp/intermediate1.png -i ./tmp/intermediate2.png -i ./tmp/intermediate3.png -i ./tmp/intermediate4.png -i ./tmp/intermediate5.png -i ./tmp/intermediate6.png -i ./tmp/intermediate7.png -i ./tmp/intermediate8.png -i ./tmp/intermediate8.png -filter_complex "[0:v][1:v][2:v][3:v][4:v][5:v][6:v][7:v][8:v]xstack=inputs=9:layout=0_0|0_h0|0_h0+h1|w0_0|w0_h0|w0_h0+h1|w0+w3_0|w0+w3_h0|w0+w3_h0+h1[v]" -map [v] output.png
+    }
 }
-
-
-
-
-module.exports = Prevvy;
+export default Prevvy;
